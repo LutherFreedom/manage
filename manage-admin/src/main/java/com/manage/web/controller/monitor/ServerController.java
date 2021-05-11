770c392803389863e103b141cd2cb0d5ba1ad757
lvpeng <lvpeng@dashuf.com>
2021-05-11 09:55:44 +0800

    清除ruoyi痕迹

---
+++ manage-admin/src/main/java/com/manage/web/controller/monitor/ServerController.java
@@ -0,0 +1,27 @@
+package com.manage.web.controller.monitor;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.manage.common.core.domain.AjaxResult;
+import com.manage.framework.web.domain.Server;
+
+/**
+ * 服务器监控
+ * 
+ * @author ruoyi
+ */
+@RestController
+@RequestMapping("/monitor/server")
+public class ServerController
+{
+    @PreAuthorize("@ss.hasPermi('monitor:server:list')")
+    @GetMapping()
+    public AjaxResult getInfo() throws Exception
+    {
+        Server server = new Server();
+        server.copyTo();
+        return AjaxResult.success(server);
+    }
+}
